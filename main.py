e419468c922c4834bdf72f34d58cba8919992267
srineha27 <indukuri.neha@gmail.com>
2018-04-07 17:27:47 -0500

    finish user-signup

---
+++ main.py
@@ -0,0 +1,56 @@
+
+from flask import Flask, request, redirect, render_template
+import re
+app = Flask(__name__)
+app.config['DEBUG'] = True
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    username = ''
+    email = ''
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+    title = 'SignUp'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        email = request.form['email']
+
+        if username == " ":
+                username_error = 'Invalid username'
+                username = ''
+        else:
+            if (len(username) < 3) or (len(username) > 20):
+                username_error = 'Invalid username'
+                username = ''
+        if password == " " :
+                password_error = 'Invalid passcode.'
+        else:
+                if (len(password)< 3) or (len(password) > 20):
+                    password_error = 'Invaild password'
+        if not len(password):
+            password_error = 'Invalid password'
+        if (verify.strip()==""):
+             verify_error = 'Passwords do not match.'
+        else:
+            if password != verify:
+                verify_rror = 'Passwords do not match.'
+          
+        if (email != '') and (not re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$', email)):
+            email_error = 'Invalid email'
+            email = ''
+        if (not username_error) and (not password_error) and (not verify_error) and (not email_error):
+            return redirect('/valid-signup?username={0}'.format(username))
+    return render_template('base.html', title=title, username=username, email=email,
+                           username_error=username_error, password_error=password_error,
+                           verify_error=verify_error, email_error=email_error)
+@app.route('/valid-signup')
+def valid_signup():
+    title = "Welcome!"
+    username = request.args.get('username')
+    return render_template('validsignup.html', title=title, username=username)
+if __name__ == '__main__':
+ app.run()
+
